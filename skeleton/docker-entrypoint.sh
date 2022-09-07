c2601cb68cac1973c5b0a8a31e6c5c55a6bbc6db
gnuamua <andrew@hoa-colors.com>
2022-09-07 05:46:23 -0600

    Re-wrote entire Dockerfile to use fedora 36 image as base for plone pip installation. The Dockerfile uses requirements.txt to install plone addons. It also copies hte skeleton dir into the image which contains files for configuring the zeo client for the plone site, as well as the 'cors' or cross origin resource security settings.

---
+++ skeleton/docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+# CLIENT HOME
+#CLIENT_HOME="/data/$(hostname)/$(hostid)"
+#export CLIENT_HOME=$CLIENT_HOME
+
+# Create directories to be used by Plone
+mkdir -p /data/filestorage /data/blobstorage /data/cache /data/log /data
+
+# MAIN ENV Vars
+[ -z ${SECURITY_POLICY_IMPLEMENTATION+x} ] && export SECURITY_POLICY_IMPLEMENTATION=C
+[ -z ${VERBOSE_SECURITY+x} ] && export VERBOSE_SECURITY=off
+[ -z ${DEFAULT_ZPUBLISHER_ENCODING+x} ] && export DEFAULT_ZPUBLISHER_ENCODING=utf-8
+[ -z ${DEBUG_MODE+x} ] && export DEBUG_MODE=off
+
+# ZODB ENV Vars
+[ -z ${ZODB_CACHE_SIZE+x} ] && export ZODB_CACHE_SIZE=50000
+
+MSG="Using ZEO configuration"
+# Check ZEO variables
+[ -z ${ZEO_SHARED_BLOB_DIR+x} ] && export ZEO_SHARED_BLOB_DIR=off
+[ -z ${ZEO_READ_ONLY+x} ] && export ZEO_READ_ONLY=false
+[ -z ${ZEO_CLIENT_READ_ONLY_FALLBACK+x} ] && export ZEO_CLIENT_READ_ONLY_FALLBACK=false
+[ -z ${ZEO_STORAGE+x} ] && export ZEO_STORAGE=1
+[ -z ${ZEO_CLIENT_CACHE_SIZE+x} ] && export ZEO_CLIENT_CACHE_SIZE=128MB
+[ -z ${ZEO_DROP_CACHE_RATHER_VERIFY+x} ] && export ZEO_DROP_CACHE_RATHER_VERIFY=false
+
+# Handle CORS
+/app/bin/python /app/scripts/cors.py
+
+if [[ "$1" == "start" ]]; then
+  echo $MSG
+  exec /app/bin/runwsgi -v /app/etc/zope.ini config_file=zeo.conf
+else
+  exec "$@"
+fi
